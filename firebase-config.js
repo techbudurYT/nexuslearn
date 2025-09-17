78109e8a450033dcbd21bfba638bc1413e77b4b0
techbudurYT <98665965+techbudurYT@users.noreply.github.com>
2025-09-17 21:52:39 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+// DİKKAT: Bu bilgileri Firebase projenizden alıp buraya yapıştırmanız gerekmektedir.
+// Firebase konsolunda projenizi oluşturduktan sonra,
+// Proje Ayarları > Genel sekmesinde "Web Uygulaması" oluşturarak bu kodları alabilirsiniz.
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBGPIimjEzzD4g-U2tuptBH5D3JlfeaiME",
+  authDomain: "nexuslearn-6ea24.firebaseapp.com",
+  projectId: "nexuslearn-6ea24",
+  storageBucket: "nexuslearn-6ea24.firebasestorage.app",
+  messagingSenderId: "544636738696",
+  appId: "1:544636738696:web:95be2c5bccebd5c05e133d",
+  measurementId: "G-GW0NYZYP7Z"
+};
+
+
+// Firebase'i başlat
+firebase.initializeApp(firebaseConfig);
